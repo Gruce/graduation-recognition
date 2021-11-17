3cc1bc76e5d8773521d4c07fbdd0af34a7e46c90
Hassan Alkhalidi <gruceing@gmail.com>
2021-11-17 13:03:10 +0300

    asdq

--- # RESET DATABASE #.bat
+++ # RESET DATABASE #.bat
@@ -1,15 +1,21 @@
-:: Name:     Reset Database
-:: Purpose:  Resetting database and reseeding it with default values
+:: Name:     Run Laravel Server
+:: Purpose:  Starting Laravel Server
 :: Author:   gruceing@gmail.com
 :: Revision: Novemeber 2021 - initial version
 
+
 @echo off
 
-
-ECHO Laravel Starting...
 cd Core
 cd Laravel
-php artisan serve
 
+ECHO Laravel reseting database...
+php artisan migrate:reset
+
+ECHO Laravel generating tables...
+php artisan migrate
+
+ECHO Laravel seeding database...
+php artisan db:seed
 
 pause
--- [1] Run Laravel Server.bat
+++ [1] Run Laravel Server.bat
@@ -1,21 +1,15 @@
-:: Name:     Run Laravel Server
-:: Purpose:  Starting Laravel Server
+:: Name:     Reset Database
+:: Purpose:  Resetting database and reseeding it with default values
 :: Author:   gruceing@gmail.com
 :: Revision: Novemeber 2021 - initial version
 
-
 @echo off
 
+
+ECHO Laravel Starting...
 cd Core
 cd Laravel
+php artisan serve
 
-ECHO Laravel reseting database...
-php artisan migrate:reset
-
-ECHO Laravel generating tables...
-php artisan migrate
-
-ECHO Laravel seeding database...
-php artisan db:seed
 
 pause

ade0fb5bc49dd942d66a046e7dd07502a0b7bb3d
hassan_000 <salem.cf11@gmail.com>
2021-11-26 00:24:16 +0300

    z

--- Core/MobApp/lib/screens/admin/home_page/home_screen.dart
+++ Core/MobApp/lib/screens/admin/home_page/home_screen.dart
@@ -1,10 +1,24 @@
-import 'package:flutter/material.dart';
-
-class AdminHomeScreen extends StatelessWidget {
-  const AdminHomeScreen({Key? key}) : super(key: key);
-
+class MaterialSearchDelegate extends PlatformSearchDelegate {
   @override
-  Widget build(BuildContext context) {
-    return Container();
+  Widget buildScaffold(Widget body, BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: buildLeading(context),
+        title: TextField(
+          controller: queryTextController,
+          focusNode: focusNode,
+          textInputAction: TextInputAction.search,
+          onSubmitted: (String _) {
+            showResults(context);
+          },
+          decoration: InputDecoration(hintText: searchFieldLabel),
+        ),
+        actions: buildActions(context),
+      ),
+      body: AnimatedSwitcher(
+        duration: const Duration(milliseconds: 300),
+        child: body,
+      ),
+    );
   }
 }

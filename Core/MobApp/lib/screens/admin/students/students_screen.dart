fa405f3d778319e25a289db69da8fc1d1b123630
Abdulkareem-bit <71324380+Abdulkareem-bit@users.noreply.github.com>
2021-12-02 15:11:17 +0300

    dd

--- Core/MobApp/lib/screens/admin/students/students_screen.dart
+++ Core/MobApp/lib/screens/admin/students/students_screen.dart
@@ -4,11 +4,11 @@ import 'package:graduaiton_app/controllers/Admin/admin_people_controller.dart';
 
 import 'package:graduaiton_app/routes/routes.dart';
 import 'package:graduaiton_app/screens/admin/people/person_widget.dart';
+import 'package:graduaiton_app/screens/admin/students/students_dropdown_widget.dart';
 import 'package:graduaiton_app/screens/admin/students/students_search_widget.dart';
 
 import '../layout.dart';
-import 'dropdown.dart';
-import 'dropdown_widget.dart';
+import 'students_dropdown_widget.dart';
 
 class AdminStudentsScreen extends GetView<AdminPeopleController> {
   const AdminStudentsScreen({Key? key}) : super(key: key);
@@ -21,9 +21,17 @@ class AdminStudentsScreen extends GetView<AdminPeopleController> {
           children: [
             // Search Container
             StudentsSearchWidget(),
-             DropDown(),
+            // DropDownWidget(),
             const SizedBox(height: 10),
-
+            Expanded(
+                child: Container(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: const Color.fromRGBO(255, 255, 255, .5)),
+              child: const DropDownWidget(),
+            )),
             Expanded(
                 child: Container(
               padding:

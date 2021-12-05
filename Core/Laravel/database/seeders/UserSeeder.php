f9c1eb8cc880d52b1b0e75e95736c52f756f4190
hmh_6 <www.hussamcr7@gmail.com>
2021-12-06 01:31:48 +0300

    Update UserSeeder.php

--- Core/Laravel/database/seeders/UserSeeder.php
+++ Core/Laravel/database/seeders/UserSeeder.php
@@ -66,7 +66,7 @@ class UserSeeder extends Seeder
             'email_verified_at' => now(),
             'password' => bcrypt('123456'),
             'remember_token' => Str::random(10),
-            'type' => 2,
+            'type' => 3,
         ]);
     }
 }

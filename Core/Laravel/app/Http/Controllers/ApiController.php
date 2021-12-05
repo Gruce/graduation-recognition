8683391871e15ad3a64eb2f003ed1493df18d4ca
hassan_000 <salem.cf11@gmail.com>
2021-12-05 20:17:38 +0300

    Revert "Merge remote-tracking branch 'origin/abdulkareem' into hassan_hazim000000"
    This reverts commit de1242627bb5296ffc0f06ae653f3e203c0844f7, reversing
    changes made to 871dc58ecbfc323deb810aa2d92df9f4d99391fd.

--- Core/Laravel/app/Http/Controllers/ApiController.php
+++ Core/Laravel/app/Http/Controllers/ApiController.php
@@ -10,13 +10,17 @@ use App\Models\Person;
 use App\Models\Camera;
 use App\Models\Tracking;
 use App\Models\Setting;
-use App\Models\Stage;
-use App\Models\Unit;
-use App\Models\Section;
-
 
 class ApiController extends Controller
 {
+
+    // USER
+    public function people(){
+        return response()->json([
+            'data' => Person::where('training_id', '!=', 0)->get(['id', 'name', 'training_id', 'type'])
+        ], 200);
+    }
+
     public function person(Request $req){
         return response()->json([
             'data' => Person::find($req->id, ['id', 'name', 'training_id', 'type'])
@@ -41,38 +45,67 @@ class ApiController extends Controller
         return response()->json(['id' => $user->id], 200);
     }
 
-    public function app_restart(Request $req){
-        $key = $req->key == 'app_restart' ? 'app_restart' : false ;
-        $data = 'Key Not Active';
-        $rsp = 401;
-        if($key){
-            $x = Setting::where('key' , $key)->first();
-            $value = $x->value == '0' ? '1' : '0';
-            $x->update(['value' => $value]);
-            $data = 'Success';
-            $rsp = 200;
-        } 
 
-        return response()->json(['data' => $data], $rsp);
-
-    }
-
-    public function sections(){
+    // CAMERA
+    public function cameras(){
         return response()->json([
-            'data' => Section::get(['id','name'])
+            'data' => Camera::where('state', '!=', 0)->get()
         ], 200);
     }
 
+    public function camera_state(Request $req){
+        $camera = Camera::find($req->id);
+        $camera->update([
+            'state' => $req->state
+            // 'state' => !$camera->state
+        ]);
 
-    public function stages(){
-        return response()->json([
-            'data' => Stage::with('section:id,name')->get(['id','name','section_id'])
-        ], 200);
+        return response()->json(['data' => 'Success.'], 200);
     }
 
-    public function units(){
-        return response()->json([
-            'data' => Unit::with(['section:id,name','stage:id,name,section_id'])->get(['id','name','section_id','stage_id'])
-        ], 200);
+    // Tracking
+    public function new_track(Request $req){
+        $file = '';
+        $name = '';
+        if ($req->image)
+            $file = $req->image->store('tracking/' . date("Y-m-d"), 'public');
+        
+        foreach (json_decode($req->people) as $person){
+            if ($person == -1){
+                $user = new Person;
+                $user->name = '';
+                $user->training_id = 1;
+                $user->type = -1;
+                $user->save();
+                $user->name = 'Unkown ' . $user->id;
+                $user->save();
+                $person = $user->id;
+
+                $name = Person::find($person)->name;
+
+                $file = $req->image->store('db/' . $user->id, 'public');
+            } else {
+                $name = Person::find($person)->name;
+            }
+
+            $track              = new Tracking;
+            $track->person_id   = $person;
+            $track->camera_id   = $req->id;
+            $track->image_path  = $file;
+            $track->save();
+        }
+        
+        return response()->json(['data' => $name], 200);
+    }
+
+    public function get_statue(){
+        $x = Setting::where('key' , 'app_restart')->first();
+        return response()->json(['data' => $x->value], 200);
+    }
+
+    public function set_statue(){
+        $x = Setting::where('key' , 'app_restart')->first();
+        $x->update(['value' => '0']);
+        return response()->json(['data' => 'Success'], 200);
     }
 }
--- Core/MobApp/lib/screens/admin/students/students_screen.dart
+++ Core/MobApp/lib/screens/admin/students/students_screen.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:graduaiton_app/controllers/Admin/admin_students_controller.dart';
+import 'package:graduaiton_app/controllers/Admin/admin_people_controller.dart';
+
 import 'package:graduaiton_app/routes/routes.dart';
-import 'package:graduaiton_app/screens/admin/students/student_widget.dart';
+import 'package:graduaiton_app/screens/admin/people/person_widget.dart';
 import 'package:graduaiton_app/screens/admin/students/students_search_widget.dart';
 import '../layout.dart';
 import 'dropdown_widget/section_dropdown_widget.dart';
 import 'dropdown_widget/stage_dropdown_widget.dart';
-import 'dropdown_widget/test.dart';
 import 'dropdown_widget/unit_dropdown_widget.dart';
-import 'section_dropdown_widget.dart';
 
-class AdminStudentsScreen extends GetView<AdminStudentsController> {
+class AdminStudentsScreen extends GetView<AdminPeopleController> {
   const AdminStudentsScreen({Key? key}) : super(key: key);
 
   @override
@@ -21,37 +20,44 @@ class AdminStudentsScreen extends GetView<AdminStudentsController> {
         child: Column(
           children: [
             // Search Container
-            // StudentsSearchWidget(),
-            SectionsDropDownWidget(),
+            StudentsSearchWidget(),
             // DropDownWidget(),
             const SizedBox(height: 20),
-      //       Container(
-      //         width: 200.0,
-      //          decoration: BoxDecoration(
-      //   color: Colors.white.withOpacity(1),
-      //   border: Border.all(
-      //     color: Colors.white.withOpacity(1),
-      //     width: 1.0,
-      //   ),
-      //   borderRadius: BorderRadius.circular(20.0),
-      // ),
-      //         // color: Colors.blue[300],
-      //         child: Row(
-      //             mainAxisAlignment: MainAxisAlignment.center,
+            Container(
+              width: 300.0,
+               decoration: BoxDecoration(
+        color: Colors.white.withOpacity(1),
+        border: Border.all(
+          color: Colors.white.withOpacity(1),
+          width: 1.0,
+        ),
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+              // color: Colors.blue[300],
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   
-      //             children: const [
-                      
-      //               SectionDropDown(),
-      //               SizedBox(width: 20,),
-      //               //  SectionDropDown(),
-      //               StageDropDown(),
-      //               SizedBox(width: 20,),
-      //               //  SectionDropDown(),
-      //               UnitDropDown(),
-      //             ]),
-      //       ),
+                  children: const [
+                    SectionDropDown(),
+                    SizedBox(width: 20,),
+                    //  SectionDropDown(),
+                    StageDropDown(),
+                    SizedBox(width: 20,),
+                    //  SectionDropDown(),
+                    UnitDropDown(),
+                  ]),
+            ),
 
             const SizedBox(height: 20),
+            // Expanded(
+            //     child: Container(
+            //   padding:
+            //       const EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+            //   decoration: BoxDecoration(
+            //       borderRadius: BorderRadius.circular(20),
+            //       color: const Color.fromRGBO(255, 255, 255, .5)),
+            //   child: const DropDownWidget(),
+            // )),
             Expanded(
                 child: Container(
               padding:
@@ -59,26 +65,22 @@ class AdminStudentsScreen extends GetView<AdminStudentsController> {
               decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(20),
                   color: const Color.fromRGBO(255, 255, 255, .5)),
-              child: GetBuilder<AdminStudentsController>(
-                  builder: (_) => controller.filteredStudents.isNotEmpty
+              child: GetBuilder<AdminPeopleController>(
+                  builder: (_) => controller.filteredPeople.isNotEmpty
                       ? ListView.builder(
-                          itemCount: controller.filteredStudents.length,
+                          itemCount: controller.filteredPeople.length,
                           itemBuilder: (BuildContext context, int index) {
                             return GestureDetector(
                                 onTap: () => Get.toNamed(Routes.personProfile,
                                     arguments:
-                                        controller.filteredStudents[index].id),
-                                child: StudentWidget(
+                                        controller.filteredPeople[index].id),
+                                child: PersonWidget(
                                     key: ObjectKey(
-                                        controller.filteredStudents[index].id),
-                                    name: controller.filteredStudents[index].user.name,
-                                    section_name:controller.filteredStudents[index].section.name,
-                                    stage_name:controller.filteredStudents[index].stage.name,
-                                    unit_name:controller.filteredStudents[index].unit.name,
+                                        controller.filteredPeople[index].id),
+                                    name: controller.filteredPeople[index].name,
+                                    type: controller.filteredPeople[index].type,
                                     camera: "Camera",
-                                    time: "Time",
-                                    user_id: controller.filteredStudents[index].user_id,
-                                     ));
+                                    time: "Time"));
                           },
                         )
                       : SizedBox(
@@ -87,8 +89,7 @@ class AdminStudentsScreen extends GetView<AdminStudentsController> {
                               padding: const EdgeInsets.symmetric(
                                   horizontal: 10.0, vertical: 5.0),
                               child: const Text('Not Found',
-                                  style: TextStyle(fontSize: 24))))
-                                  ),
+                                  style: TextStyle(fontSize: 24))))),
             ))
           ],
         ));

e4a6eed0c2bbc68c9d8659cd8cc42690bf16c29a
hmh_6 <www.hussamcr7@gmail.com>
2021-12-07 22:40:34 +0300

    update

--- Core/Laravel/database/seeders/StudentSeeder.php
+++ Core/Laravel/database/seeders/StudentSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Student;
+use App\Models\Unit;
+use App\Models\User;
+use Str;
+
 
 class StudentSeeder extends Seeder
 {
@@ -14,26 +18,40 @@ class StudentSeeder extends Seeder
      */
     public function run()
     {
+
         Student::create([
-            'user_id' => 3,
+            'user_id' => 2,
             'section_id' => 1,
             'stage_id' => 4,
             'unit_id' => 1,
         ]);
 
         Student::create([
-            'user_id' => 4,
+            'user_id' => 3,
             'section_id' => 2,
             'stage_id' => 8,
-            'unit_id' => 3,
+            'unit_id' => 15,
         ]);
 
-        Student::create([
-            'user_id' => 5,
-            'section_id' => 1,
-            'stage_id' => 4,
-            'unit_id' => 2,
-        ]);
+        foreach(Unit::get() as $item){
+            for($i = 1 ; $i <= 2 ; $i++){
+                $name = str_random(8);
+                $user = User::create([
+                    'name' => $name,
+                    'email' => $name . '@gmail.com',
+                    'email_verified_at' => now(),
+                    'password' => bcrypt('123456'),
+                    'remember_token' => Str::random(10),
+                    'type' => 2,
+                ]);
 
+                Student::create([
+                    'user_id' => $user->id,
+                    'section_id' => $item->section_id,
+                    'stage_id' => $item->stage_id,
+                    'unit_id' => $item->id,
+                ]);
+            }
+        }
     }
 }
--- Core/Laravel/database/seeders/SubjectSeeder.php
+++ Core/Laravel/database/seeders/SubjectSeeder.php
@@ -14,26 +14,37 @@ class SubjectSeeder extends Seeder
      */
     public function run()
     {
+        // ID 1
         Subject::create([
             'name' => 'C++',
             'section_id' => 1, 
             'stage_id' => 1,
         ]);
 
+        // ID 2
         Subject::create([
             'name' => 'JAVA',
             'section_id' => 1,
             'stage_id' => 2,
         ]);
 
+        // ID 3
         Subject::create([
             'name' => 'PHP',
             'section_id' => 1, 
-            'stage_id' => 3,
+            'stage_id' => 4,
         ]);
 
+        // ID 4
         Subject::create([
             'name' => 'DATA WHEREHOUSE',
+            'section_id' => 2, 
+            'stage_id' => 8,
+        ]);
+
+        // ID 5
+        Subject::create([
+            'name' => 'SOFTWARE',
             'section_id' => 2, 
             'stage_id' => 8,
         ]);

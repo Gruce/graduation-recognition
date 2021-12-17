dace906f2fb9255ddc367d2133bb13046a82bfb3
hassan_000 <salem.cf11@gmail.com>
2021-12-17 17:52:57 +0300

    z

--- Core/MobApp/lib/screens/teacher/notification/notification_screen.dart
+++ Core/MobApp/lib/screens/teacher/notification/notification_screen.dart
@@ -5,6 +5,7 @@ import 'package:graduaiton_app/controllers/Teacher/notification_controller.dart'
 import 'package:graduaiton_app/screens/teacher/home_page/widgets/button.dart';
 import 'package:graduaiton_app/screens/teacher/layout.dart';
 import 'package:file_picker/file_picker.dart';
+import 'package:graduaiton_app/screens/teacher/notification/notif.dart';
 
 class TeacherNotification extends GetView {
   TeacherNotification({Key? key}) : super(key: key);
@@ -15,123 +16,174 @@ class TeacherNotification extends GetView {
   Widget build(BuildContext context) {
     return TeacherLayoutScreen(
         title: 'Notification',
-        child: Column(
+        child:Column(
           children: [
-            FloatingActionButton.extended(
-              backgroundColor: Color(0xff6875F5),
-              onPressed: () {
-                showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return AlertDialog(
-                        scrollable: true,
-                        title: Text('Enter your notification you wnat to send'),
-                        insetPadding: EdgeInsets.zero,
-                        content: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Form(
-                            child: Column(
-                              children: <Widget>[
-                                TextFormField(
-                                  decoration: InputDecoration(
-                                    labelText: 'Title',
-                                    icon: Icon(Icons.account_box),
-                                    border: const OutlineInputBorder(),
+            GestureDetector(
+              onTap:() =>Get.to(Notif()),
+              child: Container(
+                          width: 170,
+                          height: 50,
+                          padding: EdgeInsets.all(10),
+                          decoration: const BoxDecoration(
+                              color:Color(0xff6875F5),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(20))
                                   ),
-                                  controller: controller.titleController,
-                                ),
-                                SizedBox(
-                                  height: 4,
-                                ),
-                                TextField(
-                                  controller: controller.bodyController,
-                                  maxLines: 2,
-                                  textCapitalization:
-                                      TextCapitalization.sentences,
-                                  decoration: InputDecoration(
-                                    labelText: 'Message',
-                                    icon: Icon(Icons.message),
-                                    border: const OutlineInputBorder(),
-                                  ),
-                                ),
-                                Column(
-                                  children: <Widget>[
-                                    Obx(() => Column(
-                                          children: [
-                                            Text('files count ' +
-                                                controller.files.length
-                                                    .toString()),
-                                            ElevatedButton(
-                                              onPressed: () =>
-                                                  controller.pick_files(),
-                                              child: const Text(
-                                                  "Open file picker"),
-                                            ),
-                                            controller.files_path.isNotEmpty
-                                                ? Column(
-                                                    children: List.from(
-                                                        controller.files
-                                                            .asMap()
-                                                            .map(
-                                                                (key, value) =>
-                                                                    MapEntry(
-                                                                        key,
-                                                                        Row(
-                                                                          children: [
-                                                                            Text(value.name),
-                                                                            IconButton(
-                                                                                onPressed: () => controller.removeIndex(key),
-                                                                                icon: const Icon(Icons.close_rounded)),
-                                                                          ],
-                                                                        )))
-                                                            .values
-                                                            .toList()))
-                                                : Container(),
-                                          ],
-                                        )),
-                                    Obx(
-                                      () => Container(
-                                        width: double.maxFinite,
-                                        height: double.maxFinite,
-                                        child: ListView(
-                                          shrinkWrap: true,
-                                          children: controller
-                                              .unitsCheckbox.value.keys
-                                              .map((dynamic key) {
-                                            return CheckboxListTile(
-                                              title: Text(controller
-                                                      .units[key].stage.name +
-                                                  " " +
-                                                  controller.units[key].name),
-                                              value:
-                                                  controller.unitsCheckbox.value[key],
-                                              activeColor: Colors.pink,
-                                              checkColor: Colors.white,
-                                              onChanged: (bool? value) => controller.check(key, value),
-                                            );
-                                          }).toList(),
-                                        ),
-                                      ),
-                                    )
-                                  ],
-                                ),
-                              ],
-                            ),
+                          child:Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                            
+                          Text("Send Notification",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold)),
+                          SizedBox(width: 5,),
+                          Icon(Icons.mail,color: Colors.white,)
+                          ],)
+                          
                           ),
-                        ),
-                        actions: [
-                          MyButton(
-                            label: 'Send',
-                            onTap: () => controller.send_notification(),
-                          )
-                        ],
-                      );
-                    });
-              },
-              icon: Icon(Icons.phone_android),
-              label: Text("Add Notification"),
             ),
           ],
-        ));
-  }
+        )
+        
+        
+        
+       
+        // child: Column(
+        //   children: [
+        //     FloatingActionButton.extended(
+        //       backgroundColor: Color(0xff6875F5),
+        //       onPressed: () {
+        //         showDialog(
+        //             context: context,
+        //             builder: (BuildContext context) {
+        //               return AlertDialog(
+        //                 scrollable: true,
+        //                 title: Text('Enter your notification you wnat to send'),
+        //                 insetPadding: EdgeInsets.zero,
+        //                 content: Padding(
+        //                   padding: const EdgeInsets.all(8.0),
+        //                   child: Form(
+        //                     child: Column(
+        //                       children: <Widget>[
+        //                         TextFormField(
+        //                           decoration: InputDecoration(
+        //                             labelText: 'Title',
+        //                             icon: Icon(Icons.account_box),
+        //                             border: const OutlineInputBorder(),
+        //                           ),
+        //                           controller: controller.titleController,
+        //                         ),
+        //                         SizedBox(
+        //                           height: 4,
+        //                         ),
+        //                         TextField(
+        //                           controller: controller.bodyController,
+        //                           maxLines: 2,
+        //                           textCapitalization:
+        //                               TextCapitalization.sentences,
+        //                           decoration: InputDecoration(
+        //                             labelText: 'Message',
+        //                             icon: Icon(Icons.message),
+        //                             border: const OutlineInputBorder(),
+        //                           ),
+        //                         ),
+        //                         Column(
+        //                           children: <Widget>[
+        //                             Obx(() => Column(
+        //                                   children: [
+        //                                     Text('files count ' +
+        //                                         controller.files.length
+        //                                             .toString()),
+        //                                     GestureDetector(
+        //                                       onTap:() => controller.pick_files(), 
+        //                                       child: Container(
+
+        //                                         width: 200,
+        //                                         decoration: BoxDecoration(
+        //                                               color: Colors.amber,
+        //                                             borderRadius:
+        //                                                 BorderRadius.all(
+        //                                                     Radius.circular(20))),
+        //                                         child: Row(
+        //                                           mainAxisAlignment:
+        //                                               MainAxisAlignment.center,
+        //                                           children: [
+        //                                             Text("Upload file"),
+        //                                             IconButton(
+        //                                               onPressed:(){}, 
+        //                                               icon:
+        //                                                   Icon(Icons.upload_file),
+        //                                             ),
+        //                                           ],
+        //                                         ),
+        //                                       ),
+        //                                     ),
+        //                                     controller.files_path.isNotEmpty
+        //                                         ? Column(
+        //                                             children: List.from(
+        //                                                 controller.files
+        //                                                     .asMap()
+        //                                                     .map(
+        //                                                         (key, value) =>
+        //                                                             MapEntry(
+        //                                                                 key,
+        //                                                                 Row(
+        //                                                                   children: [
+        //                                                                     Text(
+        //                                                                       value.name,
+        //                                                                     ),
+        //                                                                     IconButton(
+        //                                                                         onPressed: () => controller.removeIndex(key),
+        //                                                                         icon: const Icon(Icons.close_rounded)),
+        //                                                                   ],
+        //                                                                 )))
+        //                                                     .values
+        //                                                     .toList()))
+        //                                         : Container(),
+        //                                   ],
+        //                                 )),
+        //                             Obx(
+        //                               () => Container(
+        //                                 width: double.maxFinite,
+        //                                 height: double.maxFinite,
+        //                                 child: ListView(
+        //                                   shrinkWrap: true,
+        //                                   children: controller
+        //                                       .unitsCheckbox.value.keys
+        //                                       .map((dynamic key) {
+        //                                     return CheckboxListTile(
+        //                                       title: Text(controller
+        //                                               .units[key].stage.name +
+        //                                           " " +
+        //                                           controller.units[key].name),
+        //                                       value: controller
+        //                                           .unitsCheckbox.value[key],
+        //                                       activeColor: Colors.pink,
+        //                                       checkColor: Colors.white,
+        //                                       onChanged: (bool? value) =>
+        //                                           controller.check(key, value),
+        //                                     );
+        //                                   }).toList(),
+        //                                 ),
+        //                               ),
+        //                             )
+        //                           ],
+        //                         ),
+        //                       ],
+        //                     ),
+        //                   ),
+        //                 ),
+        //                 actions: [
+        //                   MyButton(
+        //                     label: 'Send',
+        //                     onTap: () => controller.send_notification(),
+        //                   )
+        //                 ],
+        //               );
+        //             });
+        //       },
+        //       icon: Icon(Icons.phone_android),
+        //       label: Text("Add Notification"),
+        //     ),
+        //   ],
+        // ));
+    );}
 }

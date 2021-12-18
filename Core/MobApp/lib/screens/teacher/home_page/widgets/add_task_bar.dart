fec7abea52b7bda2000cc5f72927a8b66912cd98
Abdulkareem-bit <71324380+Abdulkareem-bit@users.noreply.github.com>
2021-12-18 19:52:00 +0300

    Revert "dd"
    This reverts commit f72c1f68110a613e21810b767b8172a87e8446e3.

--- Core/MobApp/lib/screens/teacher/home_page/widgets/add_task_bar.dart
+++ Core/MobApp/lib/screens/teacher/home_page/widgets/add_task_bar.dart
@@ -1,206 +1,256 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:get/get.dart';
-import 'package:graduaiton_app/controllers/Teacher/notification_controller.dart';
 import 'package:graduaiton_app/screens/teacher/home_page/widgets/button.dart';
 import 'package:graduaiton_app/screens/teacher/home_page/widgets/input_field.dart';
+import 'package:intl/intl.dart';
 
-class Notif extends GetView {
+class AddTaskPage extends StatefulWidget {
+  const AddTaskPage({Key? key}) : super(key: key);
+
   @override
-  NotificationController controller = Get.put(NotificationController());
+  State<AddTaskPage> createState() => _AddTaskPageState();
+}
 
+class _AddTaskPageState extends State<AddTaskPage> {
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _noteController = TextEditingController();
+  DateTime _selectedDate = DateTime.now();
+  String _endTime = "9:30 PM";
+  String _startTime = DateFormat('hh:mm a').format(DateTime.now()).toString();
+  int _selectedRemind = 5;
+  List<int> remindList = [
+    5,
+    10,
+    15,
+    20,
+  ];
+
+  String _selectedRepeat = "None";
+  List<String> repeatList = [
+    "None",
+    "Daily",
+    "Weekly",
+    "Monthly",
+  ];
+  int _selectedColor = 0;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: _appBar(context),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(6),
-            child: Container(
-              margin: const EdgeInsets.all(15),
-              child: Column(
-                children: [
-              Container(
-                margin: const EdgeInsets.only(left: 5, right: 5),
-                padding: const EdgeInsets.only(left: 5),
-                decoration: new BoxDecoration (
-                    borderRadius: new BorderRadius.all(new Radius.circular(10.0)),
-                    color: Colors.white
-                ),
-              child:TextField(
-                minLines: 1, // any number you need (It works as the rows for the textarea)
-                keyboardType: TextInputType.multiline,
-                maxLines: null,
-                    decoration: const InputDecoration(
-                      labelText: 'Subject',
-                      hintText: 'enter the subject',
-                      border:OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Color(0xff6875F5),
-                        ),),
-                    ),
-                    controller: controller.titleController,
-                  ),
+      appBar: _appBar(context),
+      body: Container(
+       color: Colors.white,
+        padding:
+            const EdgeInsets.only(left: 20, right: 20, bottom: 10, top: 10),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Add Task",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.black),
               ),
-                  Container(
-                    margin: const EdgeInsets.only(left:5,right: 5,top: 4,bottom: 10),
-                    padding: const EdgeInsets.only(left: 5),
-                    decoration: new BoxDecoration (
-                        borderRadius: new BorderRadius.all(new Radius.circular(10.0)),
-                        color: Colors.white
-                    ),
-                    child:TextField(
-                      minLines: 3, // any number you need (It works as the rows for the textarea)
-                      keyboardType: TextInputType.multiline,
-                      maxLines: null,
-                      decoration: const InputDecoration(
-                        labelText: 'Text',
-                        hintText: 'enter the Text',
-                        border:OutlineInputBorder(
-                      borderSide: BorderSide(
-                      color: Color(0xff6875F5),
-                      ),
-                    ),
-                      ),
-                      controller: controller.bodyController,
-                    ),
+              MyinputField(
+                title: "Title",
+                hint: "Enter your title",
+                controller: _titleController,
+              ),
+              MyinputField(
+                title: "Note",
+                hint: "Enter your note",
+                controller: _noteController,
+              ),
+              MyinputField(
+                title: "Date",
+                hint: DateFormat.yMd().format(_selectedDate),
+                widget: IconButton(
+                  icon: Icon(
+                    Icons.calendar_today_outlined,
+                    color: Colors.grey,
                   ),
-                  SizedBox(height: 5),
-                  Obx(
-                    () => Container(
-                      margin: const EdgeInsets.only(left: 5, right: 5),
-                      padding: const EdgeInsets.only(left: 5),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        color: Colors.grey[200],
-                      ),
-                      child: ListView(
-                        shrinkWrap: true,
-                        children: controller.unitsCheckbox.value.keys
-                            .map((dynamic key) {
-                          return CheckboxListTile(
-                            title: Text(
-                              controller.units[key].stage.name +
-                                  " / " +
-                                  controller.units[key].name,
-                              style: TextStyle(fontSize: 15),
+                  onPressed: () {
+                    _getDateFromUser();
+                  },
+                ),
+              ),
+              Row(
+                children: [
+                  Expanded(
+                      child: MyinputField(
+                          title: "Start Date",
+                          hint: _startTime,
+                          widget: IconButton(
+                            icon: Icon(
+                              Icons.access_time_rounded,
+                              color: Colors.grey,
                             ),
-                            value: controller.unitsCheckbox.value[key],
-                            activeColor: const Color(0xff6875F5),
-                            checkColor: Colors.white,
-                            onChanged: (bool? value) =>
-                                controller.check(key, value),
-                          );
-                        }).toList(),
-                      ),
-                    ),
+                            onPressed: () {
+                              _getTimeFromUser(isStartTime: true);
+                            },
+                          ))),
+                  SizedBox(
+                    width: 14,
                   ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  Obx(() => Column(
-                        children: [
-                          GestureDetector(
-                            onTap: () => controller.pick_files(),
-                            child: Container(
-                              margin: const EdgeInsets.only(left: 5, right: 5),
-                              padding: const EdgeInsets.only(left: 5),
-                              decoration: BoxDecoration(
-                                  color: Colors.grey[200],
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(15))),
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Container(
-                                      decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.circular(15),
-                                        color: Colors.grey[200],
-                                      ),
-                                      child: Row(
-                                        children: [
-                                          const Text("Select file",
-                                              style: TextStyle(
-                                                  color: Color(0xff6875F5))),
-                                          IconButton(
-                                            onPressed: () =>
-                                                controller.pick_files(),
-                                            icon: const Icon(
-                                              Icons.upload_file,
-                                              color: Color(0xff6875F5),
-                                            ),
-                                          ),
-                                        ],
-                                      )),
-                                  Container(
-                                    margin: const EdgeInsets.all(10),
-                                    padding: const EdgeInsets.all(10),
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(15),
-                                      color: Colors.grey[200],
-                                    ),
-                                    child: Text(
-                                        controller.files.length.toString() +
-                                            ' File selected'),
-                                  ),
-                                ],
-                              ),
+                  Expanded(
+                      child: MyinputField(
+                          title: "End Date",
+                          hint: _endTime,
+                          widget: IconButton(
+                            icon: Icon(
+                              Icons.access_time_rounded,
+                              color: Colors.grey,
                             ),
-                          ),
-                          controller.files_path.isNotEmpty
-                              ? Container(
-                                  width: 300,
-                                  height: 150,
-                                  child: SingleChildScrollView(
-                                    child: Column(
-                                        children: List.from(controller.files
-                                            .asMap()
-                                            .map((key, value) => MapEntry(
-                                                key,
-                                                Row(
-                                                  children: [
-                                                    Text(
-                                                      value.name,
-                                                    ),
-                                                    IconButton(
-                                                        onPressed: () =>
-                                                            controller
-                                                                .removeIndex(
-                                                                    key),
-                                                        icon: const Icon(Icons
-                                                            .close_rounded)),
-                                                  ],
-                                                )))
-                                            .values
-                                            .toList())),
-                                  ),
-                                )
-                              : Container(),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          MyButton(
-                            label: 'Send',
-                            onTap: () => controller.send_notification(),
-                          )
-                        ],
-                      )),
+                            onPressed: () {
+                              _getTimeFromUser(isStartTime: false);
+                            },
+                          )))
                 ],
               ),
-            ),
+              MyinputField(
+                  title: "Remind",
+                  hint: "$_selectedRemind minutes early",
+                  widget: DropdownButton(
+                    icon: const Icon(
+                      Icons.keyboard_arrow_down,
+                      color: Colors.grey,
+                    ),
+                    iconSize: 32,
+                    elevation: 4,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.grey,
+                    ),
+                    underline: Container(
+                      height: 0,
+                    ),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        _selectedRemind = int.parse(newValue!);
+                      });
+                    },
+                    items:
+                        remindList.map<DropdownMenuItem<String>>((int value) {
+                      return DropdownMenuItem<String>(
+                          value: value.toString(),
+                          child: Text(value.toString()));
+                    }).toList(),
+                  )),
+              MyinputField(
+                  title: "Repeat",
+                  hint: "$_selectedRepeat",
+                  widget: DropdownButton(
+                    icon: const Icon(
+                      Icons.keyboard_arrow_down,
+                      color: Colors.grey,
+                    ),
+                    iconSize: 32,
+                    elevation: 4,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.grey,
+                    ),
+                    underline: Container(
+                      height: 0,
+                    ),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        _selectedRepeat = newValue!;
+                      });
+                    },
+                    items: repeatList
+                        .map<DropdownMenuItem<String>>((String? value) {
+                      return DropdownMenuItem<String>(
+                          value: value.toString(),
+                          child: Text(
+                            value!,
+                            style: TextStyle(color: Colors.grey),
+                          ));
+                    }).toList(),
+                  )),
+              SizedBox(
+                height: 18,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  _ColorPallete(),
+                  MyButton(label: "Create Task", onTap: () => _validateDate())
+                ],
+              )
+            ],
           ),
-        ));
+        ),
+      ),
+    );
+  }
+
+  _validateDate() {
+    if (_titleController.text.isNotEmpty && _noteController.text.isNotEmpty) {
+      Get.back();
+    } else if (_titleController.text.isEmpty || _noteController.text.isEmpty) {
+      Get.snackbar("Required", "All fields are require!",
+          snackPosition: SnackPosition.BOTTOM,
+          backgroundColor: Colors.orange,
+          icon: Icon(Icons.warning_amber_rounded));
+    }
+  }
+
+  _ColorPallete() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Color",
+          style: TextStyle(
+              fontSize: 14, fontWeight: FontWeight.w600, color: Colors.grey),
+        ),
+        SizedBox(
+          height: 8.0,
+        ),
+        Wrap(
+          children: List<Widget>.generate(3, (int index) {
+            return GestureDetector(
+              onTap: () {
+                setState(() {
+                  _selectedColor = index;
+                });
+              },
+              child: Padding(
+                padding: const EdgeInsets.only(right: 8.0),
+                child: (CircleAvatar(
+                  radius: 14,
+                  backgroundColor: index == 0
+                      ? Colors.blue
+                      : index == 1
+                          ? Colors.pink
+                          : Colors.orange,
+                  child: _selectedColor == index
+                      ? Icon(
+                          Icons.done,
+                          color: Colors.white,
+                          size: 16,
+                        )
+                      : Container(),
+                )),
+              ),
+            );
+          }),
+        )
+      ],
+    );
   }
 
   _appBar(BuildContext) {
     return AppBar(
-      title: const Text(
-        'Notification Details',
-        style: TextStyle(color: Colors.black),
-      ),
       elevation: 0,
-      backgroundColor: Color(0xFAFAFA),
+      backgroundColor: Colors.white,
       leading: GestureDetector(
-        child: const Icon(
+        child: Icon(
           Icons.arrow_back,
           size: 20,
           color: Colors.black,
@@ -211,5 +261,48 @@ class Notif extends GetView {
       ),
     );
   }
+
+  _getDateFromUser() async {
+    DateTime? _pickerDate = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2015),
+      lastDate: DateTime(2121),
+    );
+    if (_pickerDate != null) {
+      setState(() {
+        _selectedDate = _pickerDate;
+        print(_selectedDate);
+      });
+    } else {
+      print("it's null or somthing is wrong");
+    }
+  }
+
+  _getTimeFromUser({required bool isStartTime}) async {
+    var pickedTime = await _showTimePicker();
+    String _formatedTime = pickedTime.format(context);
+    if (pickedTime == null) {
+      print("object");
+    } else if (isStartTime == true) {
+      setState(() {
+        _startTime = _formatedTime;
+        
+      });
+    } else if (isStartTime == false) {
+      setState(() {
+        _endTime = _formatedTime;
+      });
+    }
+  }
+
+  _showTimePicker() {
+    return showTimePicker(
+        initialEntryMode: TimePickerEntryMode.input,
+        context: context,
+        initialTime: TimeOfDay(
+          hour: int.parse(_startTime.split(":")[0]),
+          minute: int.parse(_startTime.split(":")[1].split(" ")[0]),
+        ));
+  }
 }
-                                                                                                                                          

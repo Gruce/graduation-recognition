f72c1f68110a613e21810b767b8172a87e8446e3
Abdulkareem-bit <71324380+Abdulkareem-bit@users.noreply.github.com>
2021-12-18 18:14:44 +0300

    dd

--- Core/MobApp/lib/screens/teacher/home_page/schedule_teacher.dart
+++ Core/MobApp/lib/screens/teacher/home_page/schedule_teacher.dart
@@ -1,92 +1,64 @@
-import 'package:flutter/cupertino.dart';
-import 'package:date_picker_timeline/date_picker_timeline.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
 import 'package:get/get.dart';
-import 'package:graduaiton_app/controllers/Teacher/teacher_schedule_controller.dart';
-import 'package:graduaiton_app/screens/teacher/home_page/widgets/add_task_bar.dart';
+import 'package:graduaiton_app/controllers/Teacher/notification_controller.dart';
 import 'package:graduaiton_app/screens/teacher/home_page/widgets/button.dart';
-import 'package:intl/intl.dart';
-import '../layout.dart';
+import 'package:graduaiton_app/screens/teacher/home_page/widgets/input_field.dart';
 
-class TeacherScheduleScreen extends StatefulWidget {
-  const TeacherScheduleScreen({Key? key}) : super(key: key);
-
+class Notif extends GetView {
   @override
-  _TeacherScheduleScreenState createState() => _TeacherScheduleScreenState();
-}
-
-class _TeacherScheduleScreenState extends State<TeacherScheduleScreen> {
-  @override
-  DateTime _selectedDate = DateTime.now();
-  void initSate() {
-    super.initState();
-  }
+  NotificationController controller = Get.put(NotificationController());
 
   Widget build(BuildContext context) {
-    return TeacherLayoutScreen(
-      title: 'add Task',
-      child: Column(
-        children: [
-          _addTaskBar(),
-          _addDateBar(),
-        ],
-      ),
-    );
-  }
-
-  _addDateBar() {
-    return Container(
-      margin: const EdgeInsets.only(top: 20, left: 20),
-      child: DatePicker(
-        DateTime.now(),
-        height: 100,
-        width: 80,
-        initialSelectedDate: DateTime.now(),
-        selectionColor: Colors.indigo.shade700,
-        selectedTextColor: Colors.white,
-        dateTextStyle: TextStyle(
-            fontSize: 20, fontWeight: FontWeight.w600, color: Colors.grey),
-        dayTextStyle: TextStyle(
-            fontSize: 16, fontWeight: FontWeight.w600, color: Colors.grey),
-        monthTextStyle: TextStyle(
-            fontSize: 14, fontWeight: FontWeight.w600, color: Colors.grey),
-        onDateChange: (data) {
-          _selectedDate = data;
-        },
-      ),
-    );
-  }
-
-  _addTaskBar() {
-    return Container(
-      margin: const EdgeInsets.only(left: 20, right: 20, top: 10),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Container(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  DateFormat.yMMMMd().format(DateTime.now()),
-                  style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.grey[400]),
+    return Scaffold(
+        appBar: _appBar(context),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(6),
+            child: Container(
+              margin: const EdgeInsets.all(15),
+              child: Column(
+                children: [
+              Container(
+                margin: const EdgeInsets.only(left:5,right: 5,top: 4,bottom: 10),
+                decoration: new BoxDecoration (
+                    borderRadius: new BorderRadius.all(new Radius.circular(10.0)),
+                  color: Colors.grey[200],
                 ),
-                Text(
-                  "Today",
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
+              child:TextField(
+                minLines: 1, // any number you need (It works as the rows for the textarea)
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                    decoration: const InputDecoration(
+                      labelText: 'Subject',
+                      hintText: 'enter the subject',
+                      border:OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color(0xff6875F5),
+                        ),),
+                    ),
+                    controller: controller.titleController,
                   ),
-                )
-              ],
-            ),
-          ),
-          MyButton(label: "+ Add Task", onTap: () => Get.to(AddTaskPage())),
-        ],
-      ),
-    );
-  }
-}
+              ),
+                  Container(
+                    margin: const EdgeInsets.only(left:5,right: 5,top: 4,bottom: 10),
+                    decoration: new BoxDecoration (
+                        borderRadius: new BorderRadius.all(new Radius.circular(10.0)),
+                      color: Colors.grey[200],
+                    ),
+                    child:TextField(
+                      minLines: 3, // any number you need (It works as the rows for the textarea)
+                      keyboardType: TextInputType.multiline,
+                      maxLines: null,
+                      decoration: const InputDecoration(
+                        labelText: 'Text',
+                        hintText: 'enter the Text',
+                        border:OutlineInputBorder(
+                      borderSide: BorderSide(
+                      color: Color(0xff6875F5),
+                      ),
+                    ),
+                      ),
+                      controller: controller.bodyController,
+                    ),
+                  ),])))));}}

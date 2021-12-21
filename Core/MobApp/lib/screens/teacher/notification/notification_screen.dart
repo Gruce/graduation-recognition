5cd8eacb2f9b31387340fa5f43530a42572ede37
Hassan Alkhalidi <gruceing@gmail.com>
2021-12-21 21:36:40 +0300

    qwd

--- Core/MobApp/lib/screens/teacher/notification/notification_screen.dart
+++ Core/MobApp/lib/screens/teacher/notification/notification_screen.dart
@@ -11,7 +11,6 @@ import 'package:graduaiton_app/screens/teacher/layout.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:graduaiton_app/screens/teacher/notification/notif.dart';
 import 'package:intl/intl.dart';
-
 class TeacherNotification extends GetView {
   TeacherNotification({Key? key}) : super(key: key);
 
@@ -19,11 +18,13 @@ class TeacherNotification extends GetView {
   NotificationController controller = Get.put(NotificationController());
 
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: _appBar(context),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(6),
+    return TeacherLayoutScreen(
+      title: 'Notification',
+      child: Column(
+        children: [
+          IconButton(onPressed: ()=>Get.to(Attendance()), icon: Icon(Icons.ac_unit)),
+          GestureDetector(
+            onTap: () => Get.to(Notif()),
             child: Container(
               margin: const EdgeInsets.all(15),
               child: Column(
@@ -36,230 +37,94 @@ class TeacherNotification extends GetView {
                           new BorderRadius.all(new Radius.circular(10.0)),
                       color: Colors.grey[200],
                     ),
-                    child: TextField(
-                      minLines: 1,
-                      // any number you need (It works as the rows for the textarea)
-                      keyboardType: TextInputType.multiline,
-                      maxLines: null,
-                      decoration: const InputDecoration(
-                        labelText: 'Subject',
-                        labelStyle: TextStyle(color: Colors.black54),
-                        hintText: 'enter the subject',
-                        hintStyle: TextStyle(color: Colors.black38),
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(10)),
-                          borderSide:
-                              BorderSide(color: Color(0xff6875F5), width: 1.0),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(5)),
-                            borderSide:
-                                BorderSide(width: 1, color: Color(0xff6875F5))),
-                        border: OutlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Color(0xff6875F5),
-                          ),
-                        ),
-                      ),
-                      controller: controller.titleController,
-                    ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.only(
-                        left: 5, right: 5, top: 4, bottom: 10),
-                    decoration: new BoxDecoration(
-                      borderRadius:
-                          new BorderRadius.all(new Radius.circular(10.0)),
-                      color: Colors.grey[200],
-                    ),
-                    child: TextField(
-                      minLines: 3,
-                      // any number you need (It works as the rows for the textarea)
-                      keyboardType: TextInputType.multiline,
-                      maxLines: null,
-                      decoration: const InputDecoration(
-                        labelText: 'Text',
-                        labelStyle: TextStyle(color: Colors.black54),
-                        hintText: 'enter the Text',
-                        hintStyle: TextStyle(color: Colors.black38),
-                        enabledBorder: const OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(10)),
-                          borderSide: const BorderSide(
-                              color: Color(0xff6875F5), width: 1.0),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(4)),
-                            borderSide:
-                                BorderSide(width: 1, color: Color(0xff6875F5))),
-                        border: OutlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Colors.black,
-                          ),
-                        ),
-                      ),
-                      controller: controller.bodyController,
-                    ),
-                  ),
-                  SizedBox(height: 5),
-                  // TextButton(
-                  //     onPressed: () {
-                  //       DatePicker.showDatePicker(context,
-                  //           showTitleActions: true,
-                  //           minTime: DateTime(2018, 3, 5),
-                  //           maxTime: DateTime(2019, 6, 7), onChanged: (date) {
-                  //             print('change $date');
-                  //           }, onConfirm: (date) {
-                  //             print('confirm $date');
-                  //           }, currentTime: DateTime.now(), locale: LocaleType.zh);
-                  //     },
-                  //     child: Text(
-                  //       'show date time picker (Chinese)',
-                  //       style: TextStyle(color: Colors.blue),
-                  //     )),
-                  Obx(
-                    () => Container(
-                      margin: const EdgeInsets.only(left: 5, right: 5),
-                      padding: const EdgeInsets.only(left: 5),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        color: Colors.grey[200],
-                      ),
-                      child: ListView(
-                        shrinkWrap: true,
-                        children: controller.unitsCheckbox.value.keys
-                            .map((dynamic key) {
-                          return CheckboxListTile(
-                            title: Text(
-                              controller.units[key].stage.name +
-                                  " / " +
-                                  controller.units[key].name,
-                              style: TextStyle(fontSize: 15),
-                            ),
-                            value: controller.unitsCheckbox.value[key],
-                            activeColor: const Color(0xff6875F5),
-                            checkColor: Colors.white,
-                            onChanged: (bool? value) =>
-                                controller.check(key, value),
-                          );
-                        }).toList(),
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  Obx(() => Column(
-                        children: [
-                          GestureDetector(
-                            onTap: () => controller.pick_files(),
-                            child: Container(
-                              margin: const EdgeInsets.only(left: 5, right: 5),
-                              padding: const EdgeInsets.only(left: 5),
-                              decoration: BoxDecoration(
-                                  color: Colors.grey[200],
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(15))),
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Container(
+                    Icon(
+                      Icons.mail,
+                      color: Colors.white,
+                    )
+                  ],
+                )),
+          ),
+          SizedBox(height: 10,),
+         Expanded(
+              child: GetBuilder<NotificationController>(
+                  builder: (_) => controller.notifications.isNotEmpty
+                      ? ListView.builder(
+                          itemCount: controller.notifications.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return GestureDetector(
+                                // onTap: () => Get.to(() => StudentsProfileWidget(),
+                                  // arguments: controller.students[index].id),
+                              child: Card(
+                                  elevation: 0,
+                                  clipBehavior: Clip.antiAlias,
+                                  child: Container(
+                                      padding: const EdgeInsets.all(15),
                                       decoration: BoxDecoration(
                                         borderRadius: BorderRadius.circular(15),
                                         color: Colors.grey[200],
                                       ),
                                       child: Row(
                                         children: [
-                                          const Text("Select file",
-                                              style: TextStyle(
-                                                  color: Color(0xff6875F5))),
-                                          IconButton(
-                                            onPressed: () =>
-                                                controller.pick_files(),
-                                            icon: const Icon(
-                                              Icons.upload_file,
-                                              color: Color(0xff6875F5),
-                                            ),
-                                          ),
+                                          ListTile(
+                                              title: Container(
+                                                  margin: const EdgeInsets.only(
+                                                      bottom: 15),
+                                                  child: Row(
+                                                    mainAxisAlignment:
+                                                        MainAxisAlignment
+                                                            .spaceBetween,
+                                                    children: [
+                                                      Row(children: [
+                                                        Text(controller
+                                                            .notifications[index].title
+                                                            ),
+                                                      ]),
+                                                      
+                                                      Text(controller.notifications[index].created_at,style: TextStyle(fontSize: 12),),
+                                                    ],
+                                                  )),
+                                              subtitle: Row(
+                                                mainAxisAlignment:
+                                                    MainAxisAlignment
+                                                        .spaceBetween,
+                                                children: [
+                                                  Expanded(child: Text(
+                                                   controller
+                                                            .notifications[index].body
+                                                    ,
+                                                    overflow: TextOverflow.ellipsis,
+                                                    maxLines: 1,
+                                                    style: TextStyle(
+                                                        color: Colors.black
+                                                            .withOpacity(0.6)),
+                                                  )),
+                                                  Text(
+                                                    's',
+                                                    style: TextStyle(
+                                                        color: Colors.black
+                                                            .withOpacity(0.6)),
+                                                  ),
+                                                ],
+                                              )),
                                         ],
-                                      )),
-                                  Container(
-                                    margin: const EdgeInsets.all(10),
-                                    padding: const EdgeInsets.all(10),
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(15),
-                                      color: Colors.grey[200],
-                                    ),
-                                    child: Text(
-                                        controller.files.length.toString() +
-                                            ' File selected'),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                          controller.files_path.isNotEmpty
-                              ? Container(
-                                  width: 300,
-                                  height: 150,
-                                  child: SingleChildScrollView(
-                                    child: Column(
-                                        children: List.from(controller.files
-                                            .asMap()
-                                            .map((key, value) => MapEntry(
-                                                key,
-                                                Row(
-                                                  children: [
-                                                    Text(
-                                                      value.name,
-                                                    ),
-                                                    IconButton(
-                                                        onPressed: () =>
-                                                            controller
-                                                                .removeIndex(
-                                                                    key),
-                                                        icon: const Icon(Icons
-                                                            .close_rounded)),
-                                                  ],
-                                                )))
-                                            .values
-                                            .toList())),
-                                  ),
-                                )
-                              : Container(),
-                          const SizedBox(
-                            height: 15,
-                          ),
-                          MyButton(
-                            label: 'Send',
-                            onTap: () => controller.send_notification(),
-                          )
-                        ],
-                      )),
-                ],
-              ),
-            ),
-          ),
-        ));
-  }
+                                      ))),
+                            );
+                          })
+                      : SizedBox(
+                          width: double.infinity,
+                          child: Container(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 10.0, vertical: 5.0),
+                              child: const Text('Not Found',
+                                  style: TextStyle(fontSize: 24)
+                                  )
+                                  )
+                                  )
+                                  )
+                                  )
 
-  _appBar(BuildContext) {
-    return AppBar(
-        centerTitle: true,
-        title: const Text(
-          'Notification Details',
-          style: TextStyle(color: Colors.black54),
-        ),
-        elevation: 0,
-        backgroundColor: Color(0xFAFAFA),
-        leading: GestureDetector(
-          child: const Icon(
-            Icons.arrow_back_ios,
-            size: 20,
-            color: Colors.black54,
-          ),
-          onTap: () {
-            Get.back();
-          },
-        ));
+        ]));
+        
   }
+  
 }
